62ff75b881dcde4e4aed45b19d7ec3b1ebc5878e
bhilwandeabhishek <bhilwandeabhishek@gmail.com>
2025-06-20 01:01:52 +0530

    params code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use("/api",ApiRoute);
 app.get("/neha",(req,res)=>{
   const path = "D:/codes/websites/hackathon/output/common_paths.json";
   res.sendFile(path);
-})
+});
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
