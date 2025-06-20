5f1cc1a89a2326e551cff031854cae9bfc290020
bhilwandeabhishek <bhilwandeabhishek@gmail.com>
2025-06-20 10:59:48 +0530

    comment out param apiroute

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const FunctionRouter = require('./routes/FunctionRouter');
 const ApiRoute = require("./routes/ApiRoute");
 app.use('/run', FunctionRouter);
 app.use("/api",ApiRoute);
-app.get("/neha",(req,res)=>{
-  const path = "D:/codes/websites/hackathon/output/common_paths.json";
-  res.sendFile(path);
+app.get("/",(req,res)=>{
+  res.send("server is live")
 });
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
